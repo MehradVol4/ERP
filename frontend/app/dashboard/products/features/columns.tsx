0e3bbbdc91a33e171ee77fbb9668b51b4b9fc0ad
Mehrad Khademi <mehradkhd.vol4@gmail.com>
2026-08-08 00:27:08 +0330

    add products stage 1

---
+++ frontend/app/dashboard/products/features/columns.tsx
@@ -0,0 +1,101 @@
+"use client";
+import React from "react";
+import { ColumnDef } from "@tanstack/react-table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVerticalIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import ColumnFilter from "@/components/ColumnFilter";
+
+export type Products = {
+  id: number;
+  documentId: string;
+  name: string;
+  description: string | null;
+};
+
+export type CategoryFilters = {
+  name?: string;
+  description?: string;
+};
+
+export type HandleFilterChange = (
+  key: keyof CategoryFilters,
+  value: string,
+) => void;
+
+export const columns = (
+  filters: CategoryFilters,
+  handleFilterChange: HandleFilterChange,
+  onEdit: (category: Category) => void,
+  onDelete: (documentId: string) => void,
+): ColumnDef<Category>[] => [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: () => (
+      <ColumnFilter
+        columnLabel="Name"
+        placeholder="Filter name...."
+        columnValue={filters.name || ""}
+        onChange={(val) => handleFilterChange("name", val)}
+      />
+    ),
+
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "description",
+    header: () => (
+      <ColumnFilter
+        columnLabel="Description"
+        placeholder="Filter description"
+        columnValue={filters.description || ""}
+        onChange={(val) => handleFilterChange("description", val)}
+      />
+    ),
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => (
+      <DropdownMenu>
+        <DropdownMenuTrigger
+          render={
+            <Button
+              variant="ghost"
+              className="flex size-8 text-muted-foreground data-open:bg-muted"
+              size="icon"
+            />
+          }
+        >
+          <EllipsisVerticalIcon />
+          <span className="sr-only">Open menu</span>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-32">
+          <DropdownMenuItem onClick={() => onEdit(row.original)}>
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem>Make a copy</DropdownMenuItem>
+          <DropdownMenuItem>Favorite</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            variant="destructive"
+            onClick={() => {
+              onDelete(row.original.documentId);
+            }}
+          >
+            Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    ),
+  },
+];
---
+++ frontend/app/dashboard/products/features/new.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { toast } from "sonner";
+import { Field, FieldLabel, FieldError } from "@/components/ui/field";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import axiosInstance from "@/lib/axios";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().optional(),
+});
+
+//@ts-expect-error
+
+function New({ item = null, onSuccess, isOpen }) {
+  const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    if (item) {
+      form.reset({
+        name: item.name || "",
+        description: item.description || "",
+      });
+    } else {
+      form.reset({
+        name: "",
+        description: "",
+      });
+    }
+  }, [item, isOpen, form]);
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setLoading(true);
+
+    if (item?.id) {
+      await axiosInstance.put(`/api/products/${item.documentId}`, { data: values });
+    } else {
+      await axiosInstance.post("/api/products", { data: values });
+    }
+
+    toast.success("Category created !");
+    if (onSuccess) onSuccess();
+
+    setLoading(false);
+  }
+
+  return (
+    //@ts-ignore
+    <>
+      <SheetTrigger />
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>{item?.id ? "Edit" : "Add new"} Category</SheetTitle>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-8 px-6 py-5"
+          >
+            <Field>
+              <FieldLabel htmlFor="name_1959017416">Name</FieldLabel>
+              <Input
+                id="name_1959017416"
+                placeholder="Name of the category"
+                {...form.register("name")}
+              />
+
+              <FieldError>{form.formState.errors.name?.message}</FieldError>
+            </Field>
+            <Field>
+              <FieldLabel htmlFor="description">Description</FieldLabel>
+              <Textarea
+                id="description"
+                placeholder="Category's description"
+                {...form.register("description")}
+              />
+
+              <FieldError>
+                {form.formState.errors.description?.message}
+              </FieldError>
+            </Field>
+            <Button type="submit" disabled={loading}>
+              {loading ? "Saving" : "Save changes"}
+            </Button>
+          </form>
+        </SheetHeader>
+      </SheetContent>
+    </>
+  );
+}
+
+export default New;
